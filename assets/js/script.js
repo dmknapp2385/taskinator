0bc424caf42ae6bdf7c41d22b8d37c6f5603e873
dmknapp2385 <dmknapp2385@gmail.com>
2021-11-18 12:10:55 -0700

    tasks movement function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
+var taskInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 
 var taskFormHandler = function(event) {
@@ -146,6 +148,28 @@ var taskButtonHandler = function(event) {
     }
 };
 
+var taskStatusChangeHandler = function(event) {
+    // get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
+
+    // get the currently selected option's value and convert to owercase
+    var statusValue = event.target.value.toLowerCase();
+
+    // find the parent task item elemet based on the id
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    if (statusValue === 'to do') {
+        tasksToDoEl.appendChild(taskSelected);
+    }
+    else if (statusValue === 'in progress') {
+        taskInProgressEl.appendChild(taskSelected);
+    }
+    else if (statusValue === 'completed') {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
+};
+
 
 
 pageContentEl.addEventListener("click", taskButtonHandler);formEl.addEventListener("submit", taskFormHandler);
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
